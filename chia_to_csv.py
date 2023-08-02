6c7c584436d185b93cf376acb50760a22e694784
Jose Munoz <jmunozmunoz1@outlook.com>
2023-08-02 12:47:37 -0500

    refactored chia_to_csv.py

--- chia_to_csv.py
+++ chia_to_csv.py
@@ -1,59 +1,64 @@
-import itertools
+from functools import reduce
 import csv
 import sys
 
-def get_file():
-    if len(sys.argv) > 1:
-        with open(sys.argv[1], 'r') as f:
-            return f.read()
+def parse(file):
+    return list(filter(lambda s: s != '', [line.strip() for line in file.readlines()]))
 
-    else:
-        return sys.stdin.read()
+def read_file(path_to_file):
+    with open(path_to_file, 'r') as f:
+        return parse(f)
 
-def flatten(xs):
-    return itertools.chain.from_iterable(xs)
+def chunks(lines):
+    chunk = []
+    while len(lines) > 0:
+        chunk.append(lines[:5])
+        lines = lines[5:]
 
-def into_blocks(raw_string):
-    return raw_string.split('\n\n')
+    return chunk
 
-def into_dict(xs):
-    return {xs[i]: xs[i+1] for i in range(0, len(xs), 2)}
+def find_unique(chunk):
+    return {' '.join(i[2].split()[:-2]) for i in chunk}
 
-def trim_newline(string):
-    return string.split('\n')
+def to_words(transaction_chunks):
+    return [j.split(': ') if len(j.split()) > 2 else j.split() for i in transaction_chunks for j in i]
 
-def half_string(string):
-    if ': ' in string:
-        return string.split(': ')
-    else:
-        return string.split()
+def to_dict(d):
+    xs = []
 
-def get_keys_and_index(transactions_dict):
-    return [(transactions_dict[i].keys(), i) for i in range(len(transactions_dict))]
+    while len(d) > 0:
+        xs.append(reduce(lambda x, y: {**x, **y}, d[:5]))
+        d = d[5:]
 
-def get_received(keys_and_index):
-    return filter(lambda s: 'Amount received' in s[0], keys_and_index)
+    return xs
 
-def get_sent(keys_and_index):
-    return filter(lambda s: 'Amount sent' in s[0], keys_and_index)
+def write_csv(data):
+    file_names = data.keys()
 
-def seperate_received_sent(transactions_dict):
-    return (get_received(get_keys_and_index(transactions_dict)), get_sent(get_keys_and_index(transactions_dict)))
-
-def recombine(rs_tuple, transactions_list):
-    return ([transactions_list[i[1]] for i in rs_tuple[0]], [transactions_list[i[1]] for i in rs_tuple[1]])
-
-def to_csv(filename, header, data):
-    with open(filename, 'w') as f:
-        writer = csv.DictWriter(f, fieldnames = header)
-        writer.writeheader()
-        [writer.writerow(i) for i in data]
+    for i in file_names:
+        header = data[i][0].keys()
+        with open(i+'.csv', 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=header)
+            writer.writeheader()
+            writer.writerows(data[i])
 
 def main():
-    processed = [into_dict(list(flatten(map(half_string, i)))) for i in map(trim_newline, into_blocks(get_file()))]
-    rs_tuple = recombine(seperate_received_sent(processed), processed)
-    to_csv('received.csv', rs_tuple[0][0].keys(), rs_tuple[0])
-    to_csv('sent.csv', rs_tuple[1][0].keys(), rs_tuple[1])
+    if len(sys.argv) > 1:
+        transaction_chunks = chunks(read_file(sys.argv[1]))
+    else:
+        transaction_chunks = chunks(parse(sys.stdin))
+
+    output_files = {j:[] for j in [i[:-1] for i in find_unique(transaction_chunks)]}
+
+    xs = to_dict([{i[0]:i[1]} for i in to_words(transaction_chunks)]) # converts words to dict. Ex: ['Transaction', '0xc0ffee'] -> {'Transaction': '0xc0ffee'}
+
+
+    for i in output_files.keys():
+        for j in xs:
+            if i in j.keys():
+                output_files[i].append(j)
+    
+    write_csv(output_files)
 
 if __name__ == '__main__':
     main()
